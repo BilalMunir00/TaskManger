30fb554703cfa6fa26b041aaf98654bbc1a9f1c1
BilalMunir00 <118443875+BilalMunir00@users.noreply.github.com>
2023-04-11 11:54:02 +0500

    Add files via upload

---
+++ TaskManager/Shared/TaskItem.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TaskManager.Shared
+{
+    public class TaskItem
+    {
+        public int TaskItemId { get; set; }
+        public string? TaskName { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
